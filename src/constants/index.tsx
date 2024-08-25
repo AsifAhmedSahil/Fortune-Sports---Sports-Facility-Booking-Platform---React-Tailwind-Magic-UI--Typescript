ae3df44d9744dc3d74ea179ef89a5f92f47d628f
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-25 10:28:10 +0600

    Footer and navber hamburger cross menu setup

---
+++ src/constants/index.tsx
@@ -0,0 +1,31 @@
+export const navItems = [
+    { label: "About Us", href: "/about" },
+    { label: "Contacts", href: "/contact" },
+    { label: "Facilities", href: "/facilities" },
+    { label: "Helps", href: "/help" },
+    
+  ];
+
+  export const resourcesLinks = [
+    { href: "#", text: "Getting Started" },
+    { href: "#", text: "Documentation" },
+    { href: "#", text: "Tutorials" },
+    { href: "#", text: "API Reference" },
+    { href: "#", text: "Community Forums" },
+  ];
+  
+  export const platformLinks = [
+    { href: "#", text: "Features" },
+    { href: "#", text: "Supported Devices" },
+    { href: "#", text: "System Requirements" },
+    { href: "#", text: "Downloads" },
+    { href: "#", text: "Release Notes" },
+  ];
+  
+  export const communityLinks = [
+    { href: "#", text: "Events" },
+    { href: "#", text: "Meetups" },
+    { href: "#", text: "Conferences" },
+    { href: "#", text: "Hackathons" },
+    { href: "#", text: "Jobs" },
+  ];
--- src/pages/shared/Navbar.tsx
+++ src/pages/shared/Navbar.tsx
@@ -3,8 +3,17 @@ import logoImage from "../../assets/images/logosaas.png";
 import { CiMenuBurger } from "react-icons/ci";
 import { GradualSpacingDemo } from "@/components/GradualSpacingDemo";
 import { FaLongArrowAltRight } from "react-icons/fa";
+import { useState } from "react";
+import { navItems } from "@/constants";
+import { X } from "lucide-react";
 
 export const Navbar = () => {
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
+
   return (
     <div className="bg-black">
       <div className="px-4 ">
@@ -28,8 +37,36 @@ export const Navbar = () => {
             </div>
           </div>
           <div className="border border-white border-opacity-30 h-10 w-10 inline-flex justify-center items-center rounded-sm sm:hidden">
-            <CiMenuBurger className="text-white" size={20} />
+            <button onClick={toggleNavbar}>
+              {mobileDrawerOpen ? (
+                <X className="text-white"/>
+              ) : (
+                <CiMenuBurger className="text-white" size={20} />
+              )}
+            </button>
           </div>
+
+          {/* ***************************************************************** */}
+          {mobileDrawerOpen && (
+            <div className="fixed right-0 z-20 top-20 bg-black text-white w-full p-12 flex flex-col justify-center items-center lg:hidden">
+              <ul>
+                {navItems.map((item, index) => (
+                  <li key={index} className="py-4">
+                    <a href={item.href}>{item.label}</a>
+                  </li>
+                ))}
+              </ul>
+              <div className="flex space-x-6 mt-4">
+                <Link
+                  to="/dashboard/addItems"
+                  className="py-2 px-3 rounded-md bg-gradient-to-r from-green-500 to-green-800 text-white"
+                >
+                  DashBoard
+                </Link>
+              </div>
+            </div>
+          )}
+
           <nav className=" gap-8 items-center hidden sm:flex">
             <a
               href="/about"
@@ -55,9 +92,8 @@ export const Navbar = () => {
             >
               Help
             </a>
-            
           </nav>
-          <div className="flex gap-4 hidden">
+          <div className="hidden lg:flex gap-4 ">
             <button
               type="button"
               className="text-black bg-white  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
@@ -68,9 +104,10 @@ export const Navbar = () => {
               type="button"
               className="text-white bg-gradient-to-br from-purple-900 to-purple-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 flex justify-center items-center gap-2"
             >
-                
               Sign Up
-              <span className="animate-ping duration-1"><FaLongArrowAltRight/></span>
+              <span className="animate-ping duration-1">
+                <FaLongArrowAltRight />
+              </span>
             </button>
           </div>
         </div>
