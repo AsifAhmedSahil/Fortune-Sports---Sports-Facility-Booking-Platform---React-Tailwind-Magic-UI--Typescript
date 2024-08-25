5a206445fbf197b5e9cab71760f79871a55d894f
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-25 22:09:17 +0600

    contact page design

--- src/components/GlobeDemo.tsx
+++ src/components/GlobeDemo.tsx
@@ -2,8 +2,8 @@ import Globe from "@/components/magicui/globe";
 
 export function GlobeDemo() {
   return (
-    <div className="relative bg-black flex h-full w-full  items-center justify-center overflow-hidden rounded-lg border bg-background px-40 pb-40 pt-8 md:pb-60 md:shadow-xl">
-      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-purple-800 to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+    <div className="relative bg-black flex h-full w-full  items-center justify-center overflow-hidden  bg-background px-40 pb-40 pt-8 md:pb-60 md:shadow-xl">
+      <span className="pointer-events-none whitespace-pre-wrap  bg-gradient-to-b from-purple-800 to-gray-300/80 bg-clip-text text-center text-5xl md:text-5xl lg:text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10 tracking-tighter">
         Contact Us
       </span>
       <Globe className="top-28" />
